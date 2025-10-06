46e4b974d8c5afd8be9d266266780a29783a7979
leanico <235934145+leanico@users.noreply.github.com>
2025-10-06 18:45:43 +0200

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leaherre <leaherre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 18:27:49 by leaherre          #+#    #+#             */
+/*   Updated: 2025/10/06 18:29:01 by leaherre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	(unsigned char *)dest;
+	(const unsigned char*)src;
+}
---
+++ ft_memset.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leaherre <leaherre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 18:09:58 by leaherre          #+#    #+#             */
+/*   Updated: 2025/10/06 18:22:18 by leaherre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void *ft_memset(void *s, int c, size_t n)
+{
+	unsigned char valor;
+	valor = (unsigned char)c;
+	unsigned char *p;
+	p = (unsigned char*)s;
+
+	while (n > 0)
+	{
+		*p = valor;
+		p++;
+		n--;
+	}
+	return (s);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[50] = "GeeksForGeeks is for programming geeks.";
+    printf("\nBefore memset(): %s\n", str);
+
+    ft_memset(str + 13, '.', 8*sizeof(char));
+
+    printf("After memset():  %s", str);
+    return 0;
+}
