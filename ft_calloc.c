2141da283c86f277f54953d6b9227bf638bb33c9
leanico <235934145+leanico@users.noreply.github.com>
2025-10-12 23:38:29 +0200

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,3 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void *ft_calloc(size_t count, size_t size)
+{
+    size_t total_size;
+    void *ptr;
+
+    total_size = count * size;
+    if (count != 0 && total_size / count != size)
+        return (NULL);
+
+    ptr = malloc(total_size); 
+
+    if (ptr == NULL)
+        return (NULL);
+
+    ft_bzero(ptr, total_size);
+
+    return (ptr);
+}
