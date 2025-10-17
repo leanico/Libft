6ca200046eef621e80b529e4ccd0249036a319d2
leanico <235934145+leanico@users.noreply.github.com>
2025-10-17 18:53:32 +0200

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 17:15:39 by marvin            #+#    #+#             */
+/*   Updated: 2025/10/17 17:15:39 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	str_len;
+	char	*sub;
+
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+	{
+		sub = (char *)malloc(1);
+		if (sub == NULL)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
+	}
+	if (len > str_len - start)
+		len = str_len - start;
+	sub = (char *)malloc(len + 1);
+	if (sub == NULL)
+		return (NULL);
+	ft_memcpy(sub, s + start, len);
+	sub[len] = '\0';
+	return (sub);
+}
