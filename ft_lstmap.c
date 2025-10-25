0777812bf5a2ab40762e093cc7ebc63c39115356
leanico <235934145+leanico@users.noreply.github.com>
2025-10-25 23:45:54 +0200

    Add files via upload

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,20 +14,24 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_node;
 	t_list	*new_list;
-	void	*content;
+	t_list	*new_node;
+	void	*new_content;
 
-	if (lst == NULL || f == NULL || del == NULL)
+	if (lst == NULL || f == NULL)
 		return (NULL);
 	new_list = NULL;
-	while (lst != NULL)
+	while (lst)
 	{
-		content = f(lst->content);
-		new_node = ft_lstnew(content);
-		if (!new_node)
+		new_content = f(lst->content);
+		if (new_content == NULL)
 		{
-			del(content);
+			return (NULL);
+		}
+		new_node = ft_lstnew(new_content);
+		if (new_node == NULL)
+		{
+			del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
