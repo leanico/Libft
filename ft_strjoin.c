1ccc6d3f8b46c6b4e4842dcebbe98bd3a84bafb0
leanico <235934145+leanico@users.noreply.github.com>
2025-12-13 00:58:29 +0100

    Add null checks in ft_strjoin function
    Added null checks for input strings.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,6 +19,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len3;
 	char	*s3;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
 	len3 = len1 + len2;
@@ -30,3 +32,4 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s3[len3] = '\0';
 	return (s3);
 }
+
